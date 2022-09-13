a46e55abe7277ffdd1b3815528cd78907727631e
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-09-13 14:48:06 -0700

    added data, header, footer comps

---
+++ src/data.js
@@ -0,0 +1,73 @@
+export const signs = [
+  {
+    sign: 'Aquarius',
+    dates: 'Jan 21 - Feb 19',
+    emoji: '♒'
+  },
+  
+  {
+    sign: 'Aries',
+    dates: 'Mar 21 - Apr 19',
+    emoji: '♈'
+  },
+  
+  {
+    sign: 'Cancer',
+    dates: 'Jun 21 - Jul 22',
+    emoji: '♋'
+  },
+  
+  {
+    sign: 'Capricorn',
+    dates: 'Dec 22 - Feb 19',
+    emoji: '♑'
+  },
+  
+  {
+    sign: 'Gemini',
+    dates: 'May 21 - Jun 20',
+    emoji: '♊'
+  },
+  
+  {
+    sign: 'Leo',
+    dates: 'Jul 23 - Aug 22',
+    emoji: '♌'
+  },
+  
+  {
+    sign: 'Libra',
+    dates: 'Sept 23 - Oct 22',
+    emoji: '♎'
+  },
+  
+  {
+    sign: 'Pisces',
+    dates: 'Feb 19 - Mar 20',
+    emoji: '♓'
+  },
+  
+  {
+    sign: 'Sagittarius',
+    dates: 'Nov 22 - Dec 21',
+    emoji: '♐'
+  },
+  
+  {
+    sign: 'Scorpio',
+    dates: 'Oct 23 - Nov 21',
+    emoji: '♏'
+  },
+  
+  {
+    sign: 'Taurus',
+    dates: 'Apr 20 - May 20',
+    emoji: '♉'
+  },
+  
+  {
+    sign: 'Virgo',
+    dates: 'Aug 23 - Sep 22',
+    emoji: '♍'
+  },
+];
