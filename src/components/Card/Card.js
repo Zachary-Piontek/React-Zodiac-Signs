1585e9ed5a7bf7d19aaac5ee8ce3d489430c0882
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-09-13 16:13:02 -0700

    background image and styles added

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../Card/card.css';
 
 export default function ZodiacSign(props) {
   return (
-    <div>
+    <div className='card'>
       <img src={`${process.env.PUBLIC_URL}/zodiac/${props.name}.png`} alt={props.symbol}/>
       <span>{props.name}</span>
       <span>{props.dates}</span>
--- src/components/Main.js/Main.js
+++ src/components/Main.js/Main.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { zodiac } from '../../data.js';
 import ZodiacSign from '../Card/Card.js';
-
-
+import '../Main.js/main.css';
+import background from '../../zodiac-background.png';
 
 export default function Main() {
   return (
-    <main>
+    <main style={{ backgroundImage: `url(${background})` }}>
       {zodiac.map((signs) => (
         <ZodiacSign
           key={signs.id}
