098ead7162b576faaf1249b60c181abbf1ba2a08
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-09-13 15:32:19 -0700

    images display renders

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function ZodiacSign(props) {
   return (
     <div>
+      <img src={`${process.env.PUBLIC_URL}/zodiac/${props.name}.png`} alt={props.symbol}/>
       <span>{props.name}</span>
       <span>{props.dates}</span>
       <span>{props.symbol}</span>
